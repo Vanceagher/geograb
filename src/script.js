3b6da1025b52a43816c47003432eaa7b0f67dd80
Vanceagher <48972091+Vanceagher@users.noreply.github.com>
2021-03-19 14:18:43 -0400

    Update script.js

--- src/script.js
+++ src/script.js
@@ -121,7 +121,7 @@ function showPosition(position) {
             document.getElementById("maps2").innerHTML = "https://www.google.com/maps/search/?api=1&query=" + address.replace(/\s/g, '+'); // MAPS ADDRESS
         });
     });
-    document.getElementById("street").innerHTML = "http://maps.google.com/maps?q=&layer=c&cbll=" + position.coords.latitude + ',' + position.coords.longitude + "&cbp=0,0,0,0,0";
+    document.getElementById("street").innerHTML = "https://maps.google.com/maps?q=&layer=c&cbll=" + position.coords.latitude + ',' + position.coords.longitude + "&cbp=0,0,0,0,0";
     document.getElementById("image").innerHTML = "https://static-maps.yandex.ru/1.x/?lang=en-US&ll=" + position.coords.longitude + "," + position.coords.latitude + "&z=18&l=sat&size=600,300"; // IMAGE
     document.getElementById("coords").innerHTML = position.coords.latitude + ", " + position.coords.longitude; // COORDS
     document.getElementById("maps").innerHTML = "https://www.google.com/maps/search/?api=1&query=" + position.coords.latitude + "," + position.coords.longitude; // MAPS COORDS
